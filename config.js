05ef62b33a79173a923cea91ad305d323ce50882
dewanaluminium <160332098+dewanaluminium@users.noreply.github.com>
2025-08-26 22:50:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Isi dengan URL Web App Google Apps Script kamu
-const API_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwmdlOy_9mD4MtR41Nem6qJKVWdk9HZwVBQiVklPTuIgrG707oypP1QQQnvdHBYLqqW/exec';
