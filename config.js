1cc4fdd3dd58f3d1dc554ad48f54421040e21b1b
dewanaluminium <160332098+dewanaluminium@users.noreply.github.com>
2025-08-26 22:24:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Ganti URL di bawah dengan Web App URL dari Google Apps Script deployment kamu.
 // (Contoh: 'https://script.google.com/macros/s/AKfycbx.../exec')
-const API_URL = 'https://script.google.com/macros/s/AKfycbygNZrXtK786Q-y84Y3TXwDF4gZoSLgQSh0ob799UNmGXWy23Y1SwUfImW4g54i7GBDlg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwmdlOy_9mD4MtR41Nem6qJKVWdk9HZwVBQiVklPTuIgrG707oypP1QQQnvdHBYLqqW/exec';
 
 // Opsional: jika kamu menambahkan API key sederhana di Apps Script (lihat README.md), isi di sini:
 const API_KEY = ''; // contoh: 'my-long-random-key'
